5a842185ea0abfda1cc8ec40bf05d34d046b4a03
toeiigit <toeii.git@gmail.com>
2021-09-11 21:45:44 +0700

    Change Endpoint without Replica name v.3

--- change_replicate.sh
+++ change_replicate.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-MONGODB_01=$1
-MONGODB_02=$2
-MONGODB_03=$3
+MONGODB_01="mymodockersitmsdb01.cloud"
+MONGODB_02="mymodockersitmsdb02.cloud"
+MONGODB_03="mymodockersitmsdb03.cloud"
 
 PORT_LIST=$(find . -name default.yml | xargs grep "mongodb://" | grep -v "#" | awk -F: {'print$6'} | awk -F/ {'print$1'} | sed -e '/^[[:space:]]*$/d' | sort -u)
 
 for PORT in ${PORT_LIST}
 do
-        OLD_HOST_REP=$(find . -name default.yml | xargs grep "mongodb://" | grep -v '#' | grep ${PORT} | awk -F@ {'print$2'} | awk -F: {'print$1'} | sed -e '/^[[:space:]]*$/d' | sort -u)
+        OLD_HOST_REP=$(find . -name default.yml | xargs grep "mongodb://" | grep -v "#" | awk -F@ {'print$2'} | awk -F/ {'print$1'} | grep ${PORT} | sed -e '/^[[:space:]]*$/d' | sort -u)
         for CONFIG_DBFILE in $(find . -name default.yml | xargs grep "mongodb://" | grep -v '#' | grep ${PORT} | awk -F: {'print$1'} | sort -u)
         do
-                sed -i 's/${OLD_HOST_REP}:${PORT}/${MONGODB_01}:${PORT},${MONGODB_02}:${PORT},${MONGODB_03}:${PORT}/g' ${CONFIG_DBFILE}
+            sed -i 's/${OLD_HOST_REP}/${MONGODB_01}:${PORT},${MONGODB_02}:${PORT},${MONGODB_03}:${PORT}/g' ${CONFIG_DBFILE}
+            STATUS=$(echo $?)
+            if [ ${STATUS} == '0' ]
+            then
+                echo "SUCCESS --> sed -i 's/${OLD_HOST_REP}/${MONGODB_01}:${PORT},${MONGODB_02}:${PORT},${MONGODB_03}:${PORT}/g' ${CONFIG_DBFILE}"
+            else
+                echo "Fail !!!!!"
+            fi
         done
 done
